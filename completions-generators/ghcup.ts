4da0869204d595c8190e6dae31edd304e7159fbb
Lhc_fl <lhcfl@outlook.com>
2025-09-10 20:46:58 +0800

    🎨 make the ts pure functional

--- completions-generators/ghcup.ts
+++ completions-generators/ghcup.ts
@@ -1,32 +1,18 @@
 export default async function generator() {
   const { stdout } = Bun.spawn(["ghcup", "-h"]);
   const text = await stdout.text();
-  const lines = text.split("\n");
 
-  const out : string[] = [
+  const head =
 `# disable file completion
 complete -c ghcup -f
-`,
-  ];
-  let is_command = false;
-  for (const line of lines) {
-    if (line.trim() === "") {
-      is_command = false;
-      continue;
-    }
-    if (is_command) {
-      const matched = line.trim().match(/^(\w+)\s+(.*)$/);
-      if (matched) {
-        const command = matched[1];
-        out.push(`complete -c ghcup -n '__fish_use_subcommand' -a '${command}' -d '${matched[2]}'`);
-      } else {
-        is_command = false;
-      }
-    }
-    if (line.trim().startsWith("Main commands")) {
-      is_command = true;
-    }
-  }
+`;
 
-  return out.join("\n") + "\n";
+  const [_, body] = text.match(/commands:([\s\S]*?)\n\n/)!;
+  
+  const out = body!.trim().split("\n").map(line => {
+    const [_, command, description] = line.trim().match(/^(\w+)\s+(.*)$/)!;
+    return `complete -c ghcup -n '__fish_use_subcommand' -a '${command}' -d '${description}'`;
+  });
+
+  return [head, ...out].join("\n") + "\n";
 }
